6356c1793b0c150a4d11f978ef3ea39ef3fecf6c
cacaopr <cacaopr>
2023-03-21 13:15:25 +0000

    Added clock and changed mirror function name

--- app/src/main/java/com/example/prototipotese/MatrixOperations.kt
+++ app/src/main/java/com/example/prototipotese/MatrixOperations.kt
@@ -1,32 +1,29 @@
 package com.example.prototipotese
 
 import android.util.Log
-import android.opengl.Matrix
-import androidx.core.graphics.rotationMatrix
 
-
-private val <T> Array<T>.toFloatArray: Unit
-    get() {}
 
 object MatrixOperations {
 
     fun listToMatrix(list: List<Int>, rows: Int, cols: Int): Array<Array<Int>> {
-        //require(list.size == rows * cols) { "List size doesn't match matrix size" }
         val matrix = Array(rows) { Array(cols) { 0 } }
         var index = 0
+
         for (i in 0 until rows) {
+
             for (j in 0 until cols) {
+
                 if (index < list.size) {
+
                     matrix[i][j] = list[index]
                     index++
+
                 } else {
                     break
                 }
             }
         }
-
         val matrix1 = Array(rows) { Array(cols) { 0 } }
-        var indice = 0
         var count: Int = 1
         for (h in list.indices) {
             if (list[h] / cols < 1.0) {
@@ -74,8 +71,7 @@ input / 3 < 1 => primeira row
  */
 
 
-
-        for (f in matrix.indices) {
+/*        for (f in matrix.indices) {
             //  for (g in 0 .. 2){
             Log.d(
                 javaClass.name,
@@ -96,14 +92,12 @@ input / 3 < 1 => primeira row
         }
 
         var resu: FloatArray = floatArrayOf(1.2F)
-        matrix1.toFloatArray
         //Matrix.rotateM(resu,1,matrix1,0, 90F, 0.0F, 0.0F, 0.0F)
         //println(resu.contentToString())
 
         val rotatedMatrix = rotateMatrixplus90(matrix1)
         //println(rotatedMatrix.contentDeepToString())
         for (f in rotatedMatrix.indices) {
-            //  for (g in 0 .. 2){
             Log.d(
                 javaClass.name,
                 "################# ${rotatedMatrix[f][0]},${rotatedMatrix[f][1]},${rotatedMatrix[f][2]} ################# "
@@ -114,23 +108,36 @@ input / 3 < 1 => primeira row
         val rotatedMatrix2 = rotateMatrixminus90(matrix1)
         //println(rotatedMatrix2.contentDeepToString())
         for (f in rotatedMatrix2.indices) {
-            //  for (g in 0 .. 2){
             Log.d(
                 javaClass.name,
                 "!!!!!!!!!!!!!!!!! ${rotatedMatrix2[f][0]},${rotatedMatrix2[f][1]},${rotatedMatrix2[f][2]} !!!!!!!!!!!!!!!!! "
             )
+
+        }*/
+        for (f in matrix1.indices) {
+            //  for (g in 0 .. 2){
+            Log.d(
+                javaClass.name,
+                "!!!!!!!!!!!!!!!!! ${matrix1[f][0]},${matrix1[f][1]},${matrix1[f][2]} !!!!!!!!!!!!!!!!! "
+            )
+            //}
+
         }
 
-
-
-
+        val mirrorMatrix = mirrorMatrixVert(matrix1)
+        for (f in mirrorMatrix.indices) {
+            Log.d(
+                javaClass.name,
+                "################# ${mirrorMatrix[f][0]},${mirrorMatrix[f][1]},${mirrorMatrix[f][2]} ################# "
+            )
+        }
         return matrix
     }
 
-    fun rotateMatrixplus90(matrix: Array<Array<Int>>): Array<Array<Int?>> {
+    fun rotateMatrixplus90(matrix: Array<Array<Int>>): Array<Array<Int>> {
         val rows = matrix.size
         val cols = matrix[0].size
-        val rotatedMatrix = Array(cols) { arrayOfNulls<Int>(rows) }
+        var rotatedMatrix = Array(cols) { Array(rows) { 0 } }
         for (i in 0 until rows) {
             for (j in 0 until cols) {
                 rotatedMatrix[j][rows - i - 1] = matrix[i][j]
@@ -140,33 +147,37 @@ input / 3 < 1 => primeira row
         return rotatedMatrix
     }
 
-    fun rotateMatrixminus90(matrix: Array<Array<Int>>): Array<Array<Int?>> {
+    fun rotateMatrixminus90(matrix: Array<Array<Int>>): Array<Array<Int>> {
         val rows = matrix.size
         val cols = matrix[0].size
-        val rotatedMatrix = Array(cols) { arrayOfNulls<Int>(rows) }
-        val rotatedResult = Array(cols) { arrayOfNulls<Int>(rows) }
+        var copier = matrix
+        var rotatedMatrix = Array(cols) { Array(rows) { 0 } }
+        var rotatedResult = Array(cols) { Array(rows) { 0 } }
         for (t in 1..3) {
             for (i in 0 until rows) {
                 for (j in 0 until cols) {
-                    rotatedMatrix[j][rows - i - 1] = matrix[i][j]
+                    rotatedMatrix[j][rows - i - 1] = copier[i][j]
                 }
             }
+            rotatedResult = rotatedMatrix
+            copier = rotatedMatrix
+            rotatedMatrix = Array(cols) { Array(rows) { 0 } }
         }
 
-        return rotatedMatrix
+        return rotatedResult
     }
 
-    fun <T> mirrorMatrix(matrix: Array<Array<T>>): Array<Array<T>> {
+    fun mirrorMatrixVert(matrix: Array<Array<Int>>): Array<Array<Int>> {
         val rows = matrix.size
         val cols = matrix[0].size
-        val mirroredMatrix = Array(rows) { arrayOfNulls<Any>(cols) }
+        val mirroredMatrix = Array(rows) { Array(cols) { 0 } }
         for (i in 0 until rows) {
             for (j in 0 until cols) {
                 mirroredMatrix[i][j] = matrix[i][cols - j - 1]
             }
         }
-        @Suppress("UNCHECKED_CAST")
-        return mirroredMatrix as Array<Array<T>>
+
+        return mirroredMatrix
     }
 
 
